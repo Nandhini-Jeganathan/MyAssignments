6d3c22ac369a51c5cf7ebfbb3a5febec569f0d6a
Nandhini-Jeganathan <114815343+Nandhini-Jeganathan@users.noreply.github.com>
2022-11-20 14:54:44 +0530

    Delete MultipleLangauge.java

--- MultipleLangauge.java
+++
@@ -1,8 +0,0 @@
-package week3.day1;
-
-public abstract class MultipleLangauge implements TestTool {
-		public void python() {
-			System.out.println("python");
-		}
-		public abstract void ruby(); 
-}
