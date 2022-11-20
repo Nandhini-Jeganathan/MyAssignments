c417e4c96d220ba42379c6629531165b9c728f9f
Nandhini-Jeganathan <114815343+Nandhini-Jeganathan@users.noreply.github.com>
2022-11-20 14:47:02 +0530

    Add files via upload
    Week3.day1.Assignments(1 to 5)

---
+++ week3/day1/BankInfo.java
@@ -0,0 +1,15 @@
+package week3.day1;
+
+public class BankInfo {
+
+		public void saving() {
+			System.out.println("20000");
+		}
+		public void fixed() {
+			System.out.println("6789");
+		}
+		public void deposit() {
+			System.out.println("5432");
+			
+		}
+}
---
+++ week3/day1/MultipleLangauge.java
@@ -0,0 +1,8 @@
+package week3.day1;
+
+public abstract class MultipleLangauge implements TestTool {
+		public void python() {
+			System.out.println("python");
+		}
+		public abstract void ruby(); 
+}
