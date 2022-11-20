8aba303afae336f338f17162a3fc4623a8284b6c
Nandhini-Jeganathan <114815343+Nandhini-Jeganathan@users.noreply.github.com>
2022-11-20 14:50:49 +0530

    Add files via upload
    Week3.day1.Asisgnments(1 to 5)

---
+++ week3/day1/org/college/College.java
@@ -0,0 +1,16 @@
+package org.college;
+
+public class College {
+	public void collegeName() {
+		System.out.println("K.Ramakrishnan College of Engineering");
+	}
+	public void collegeCode() {
+		System.out.println("3841");
+	}
+	public void collegeRank()
+	{
+		System.out.println("9th Rank");
+	}
+}
+
+
